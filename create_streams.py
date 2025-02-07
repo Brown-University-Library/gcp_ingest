0ac1b73cb9d5223f9609befd82836972372ac5a6
Connor Murphy <connor@murphysites.com>
2025-02-07 14:59:08 -0500

    query for parents and their namesake children, add comments about next steps

--- create_streams.py
+++ create_streams.py
@@ -17,21 +17,41 @@ def queue_create_stream_job(pid, datastream_or_url=None, visibility="BDR_PUBLIC"
     queue_job(queue_name='stream_objects', function_name='stream_objects.create', function_args=(pid,), function_kwargs=kwargs)
 
 def main():
-    response = requests.get(api_url,params)
-    if response.ok:
-        docs = response.json()["response"]["docs"]
-    if docs:
-        for doc in docs:
-            if doc["rel_is_part_of_ssim"]:
-                continue
-            print(doc["pid"])
-
-if __name__ == "__main__":
     load_dotenv()
     api_url = os.environ["SOLR_URL"]
     collection = os.environ["COLLECTION_PID"]
     params = {
-        "q":f"rel_is_member_of_collection_ssim:{collection} object_type:video",
+        "q":f"rel_is_member_of_collection_ssim:{collection}",
+        "fq":"!rel_is_part_of_ssim:['' TO *]",
         "rows":500
     }
+    response = requests.get(api_url,params)
+    if not response.ok:
+        print(f"Response not ok: {response.status_code} - {response.headers}")
+        return
+    print(f'found {response.json()["response"]["numFound"]} items...')
+    docs = response.json()["response"]["docs"]
+    for doc in docs:
+        filename = doc["mods_id_filename_ssim"][0]
+        pid = doc["pid"]
+        resp = requests.get(api_url,params={
+            "q":f'rel_is_part_of_ssim:{pid} mods_id_filename_ssim:{filename}'
+        })
+        if not resp.ok:
+            print(f"Response not ok for {pid} - {filename}: {resp.status_code} - {resp.headers}")
+            continue
+        response = resp.json()["response"]
+        if not response:
+            print(f"No response for {pid} - {filename}")
+            continue
+        if response["numFound"] != 1:
+            print(f'more than one equal child found for {pid} - {filename}: {[doc["pid"]+" - "+doc["mods_id_filename_ssim"] for doc in response["docs"]]}')
+            continue
+        item = response["docs"][0]
+        print(f"parent {pid} will be assigned stream link for item {item['pid']}")
+        # Queue job for stream creation
+        # Get stream from job? or maybe sleep & get from item?
+        # edit parent rels with stream link
+
+if __name__ == "__main__":
     main()
